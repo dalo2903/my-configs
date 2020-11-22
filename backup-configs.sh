c98076b808cb93bff67e66707c23078b1c9f6a8a
Duy Kha Dinh <khadinh@g.skku.edu>
2020-11-22 20:38:50 +0900

    add files

--- backup-configs.sh
+++ backup-configs.sh
@@ -4,6 +4,7 @@ rsync -a ~/.emacs.d . --exclude .git
 rsync -a ~/.tmux . --exclude .git
 rsync -a ~/.oh-my-zsh . --exclude .git
 
+cp ~/.p10k.zsh .
 cp ~/.zshrc .
 cp ~/.bashrc .
 cp ~/.tmux.conf .
--- restore-configs.sh
+++ restore-configs.sh
@@ -2,6 +2,7 @@ rsync -a .emacs.d ~ --exclude .git
 rsync -a .tmux ~ --exclude .git
 rsync -a .oh-my-zsh ~ --exclude .git
 
+cp .p10k.zsh ~
 cp .zshrc ~
 cp .bashrc ~
 cp .tmux.conf ~
