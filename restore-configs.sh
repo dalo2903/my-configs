86e2716ddf00f4b472c45f1c9bbcf8fa3a3e23a0
Dinh Duy Kha <dalo2903@gmail.com>
2021-01-13 18:20:44 +0900

    add

--- restore-configs.sh
+++ restore-configs.sh
@@ -1,6 +1,7 @@
 rsync -a --progress .emacs.d ~
 # rsync -a --progress .tmux ~ --exclude .git
 curl -L git.io/antigen > antigen.zsh
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # rsync -a --progress .oh-my-zsh ~ --exclude .git
 
 cp .p10k.zsh ~
